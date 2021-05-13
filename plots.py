bf3082f662f475ca8c0944697a01d10509441d47
Christian Cabrera <christiancabrera89@gmail.com>
2021-05-13 16:57:27 +0100

    updating metrics results and figures

--- plots.py
+++ plots.py
@@ -17,12 +17,11 @@ plt.rc("legend", fontsize=SMALL_SIZE)
 plt.rc("figure", titlesize=LARGE_SIZE)
 
 data = pd.read_csv("metrics.csv")
-metrics = ["halstead_volume", "halstead_difficulty", "halstead_effort", "maintainability_index",
-           "cyclomatic_complexity", "cognitive_complexity"]
-
+metrics = data.columns.tolist()
+metrics.remove("App Key")
 for metric in metrics:
-    data_fbp = data.loc[data['app_key'].isin(["fbp_app_min", "fbp_app_data", "fbp_app_ml"])]
-    data_soa = data.loc[data['app_key'].isin(["soa_app_min", "soa_app_data", "soa_app_ml"])]
+    data_fbp = data.loc[data["App Key"].isin(["fbp_app_min", "fbp_app_data", "fbp_app_ml"])]
+    data_soa = data.loc[data["App Key"].isin(["soa_app_min", "soa_app_data", "soa_app_ml"])]
     X = ["Min", "Data", "ML"]
     fig, ax = plt.subplots()
     sns.lineplot(data=data_fbp, x=X, y=metric, ax=ax, label="FBP-based", marker='o', markersize="8", linestyle=':')
@@ -42,8 +41,8 @@ for metric in metrics:
     pos_y = y[:-1] + v/2
     norm = np.sqrt(u**2+v**2)
     ax.quiver(pos_x, pos_y, u/norm, v/norm, angles="xy", zorder=5, pivot="mid")
-    ax.set(xlabel="Application Stage", ylabel=metric.replace("_", " ").title())
+    ax.set(xlabel="Application Stage", ylabel=metric)
     ax.grid(linestyle="-", linewidth="0.25", color="grey")
     ax.legend(title="Application Type", fancybox=True)
-    plt.savefig("figs/"+metric+".pdf")
+    plt.savefig("figs/"+metric.replace(" ", "_").lower()+".pdf")
 
