34dc1187f3fae2224e342c77f22554343181b0d0
Andrei Paleyes <apalees@gmail.com>
2021-08-18 16:45:02 +0100

    Generating new posts

--- mblogger/main.py
+++ mblogger/main.py
@@ -1,16 +1,21 @@
 import sys
 import random
+import datetime
 
 from .fbp_app_min import App
 from .record_types import *
+
+from essential_generators import DocumentGenerator
 
 random.seed(42)
 
 
 n_steps = 5
 n_users = 10
+
 n_requests_per_step = 3
 n_unfollows_per_step = 1
+n_posts_per_step = 3
 
 
 user_ids = [i for i in range(1, n_users + 1)]
@@ -41,15 +46,37 @@ def generate_requests():
 
     return requests
 
+def generate_posts():
+    gen = DocumentGenerator()
+
+    new_posts = []
+    for _ in range(n_posts_per_step):
+        text = gen.sentence()
+        author_id = random.choice(user_ids)
+        post_id = len(posts) + len(new_posts) + 1
+        post = Post(post_id, author_id, text, timestamp=datetime.now())
+        new_posts.append(post)
+
+    return new_posts
+
 app = App()
 
 for step in range(n_steps):
     print(f"################### Iteration {step} ###################")
-    for record in followers:
-        print(f"User {record.user_id} has {len(record.followers)} followers")
 
+    print("--- Generating data ---")
     follow_requests = generate_requests()
+    new_posts = generate_posts()
+    posts.extend(new_posts)
     app.add_data(followings, followers, follow_requests, posts)
-    output = app.evaluate()
+
+    print("--- Evaluation ---")
+    followers, followings, timelines = app.evaluate()
+
+    print("--- Stats after evaluation ---")
+    for record in followings:
+        user_id = record.user_id
+        timeline = next(t for t in timelines if t.user_id == user_id)
+        print(f"User {record.user_id} follows {len(record.followings)} users and has {len(timeline.posts)} posts in timeline")
 
     print()
