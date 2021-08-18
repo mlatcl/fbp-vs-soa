dc5950ac06901c6a2f4f0dddd4c70dde69680cf7
Andrei Paleyes <apalees@gmail.com>
2021-08-18 15:24:10 +0100

    Simple data generation routine for follow requests

--- mblogger/main.py
+++ mblogger/main.py
@@ -1,21 +1,55 @@
 import sys
+import random
 
 from .fbp_app_min import App
+from .record_types import *
+
+random.seed(42)
+
 
 n_steps = 5
+n_users = 10
+n_requests_per_step = 3
+n_unfollows_per_step = 1
+
+
+user_ids = [i for i in range(1, n_users + 1)]
+
+followings = [FollowingsRecord(user_id=i, followings=set()) for i in user_ids]
+followers = [FollowersRecord(user_id=i, followers=set()) for i in user_ids]
+posts = []
+
+def generate_requests():
+    requests = []
+
+    for _ in range(n_requests_per_step):
+        active, passive = random.sample(user_ids, 2)
+        follow_request = FollowRequest(active_author=active, passive_author=passive, follow=True)
+        requests.append(follow_request)
+    
+    for _ in range(n_unfollows_per_step):
+        followers_record = random.choice(followers)
+        if len(followers_record.followers) == 0:
+            # author has empty list of followers
+            # no unfollow requests this time then
+            break
+        active = followers_record.user_id
+        passive = random.choice(list(followers_record.followers))
+        unfollow_request = FollowRequest(active_author=active, passive_author=passive, follow=False)
+        
+        requests.append(unfollow_request)
+
+    return requests
 
 app = App()
 
 for step in range(n_steps):
     print(f"################### Iteration {step} ###################")
+    for record in followers:
+        print(f"User {record.user_id} has {len(record.followers)} followers")
 
-    # at each step we may have
-    # 1. new users
-    # 2. new follows
-    # 3. new unfollows
-    # 4. new posts
-
-    app.add_data([], [], [], [])
+    follow_requests = generate_requests()
+    app.add_data(followings, followers, follow_requests, posts)
     output = app.evaluate()
 
-    print(output)
+    print()
