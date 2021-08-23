d488f59c2f3776c8d219b1acfca9e8a310b0e21c
cabrerac <christiancabrera89@gmail.com>
2021-08-23 10:51:08 +0100

    tested version of mblogger soa

--- mblogger/soa_app_min/flaskr/data/author.py
+++ mblogger/soa_app_min/flaskr/data/author.py
@@ -5,11 +5,12 @@ import json
 # Registers a new follow in the database
 def follows_author(follow):
     db = get_db()
-    sql = 'INSERT INTO Follows (active_author, passive_author, follow) VALUES (?, ?, ?)'
-    values = [follow['active_author'], follow['passive_author'], follow['follow']]
+    sql = 'INSERT INTO Follows (active_author, passive_author, follow) VALUES (?, ?, ?) ' \
+          'ON CONFLICT(active_author, passive_author) DO UPDATE SET follow = ?'
+    values = [follow['active_author'], follow['passive_author'], follow['follow'], follow['follow']]
     cursor = db.execute(sql, values)
     db.commit()
-    return cursor.rowcount
+    return cursor.lastrowid
 
 
 # Get list of followers for an author
@@ -20,12 +21,12 @@ def get_list_followers():
     passive_authors = db.execute(sql)
     for passive_author in passive_authors:
         sql = 'SELECT active_author FROM Follows WHERE passive_author = ? AND follow = 1'
-        values = [passive_author]
+        values = [passive_author['passive_author']]
         active_authors = db.execute(sql,values)
         followers = []
         for active_author in active_authors:
-            followers.append(active_author)
-        r = {'user_id' : passive_author, 'followers': followers}
+            followers.append(active_author['active_author'])
+        r = {'user_id' : passive_author['passive_author'], 'followers': followers}
         res.append(r)
     return res
 
@@ -38,11 +39,11 @@ def get_list_followings():
     active_authors = db.execute(sql)
     for active_author in active_authors:
         sql = 'SELECT passive_author FROM Follows WHERE active_author = ? AND follow = 1'
-        values = [active_author]
+        values = [active_author['active_author']]
         passive_authors = db.execute(sql, values)
         followings = []
         for passive_author in passive_authors:
-            followings.append(passive_author)
-        r = {'user_id': passive_author, 'followings': followings}
+            followings.append(passive_author['passive_author'])
+        r = {'user_id': active_author['active_author'], 'followings': followings}
         res.append(r)
     return res
--- mblogger/soa_app_min/flaskr/data/schema.sql
+++ mblogger/soa_app_min/flaskr/data/schema.sql
@@ -1,17 +1,16 @@
 DROP TABLE IF EXISTS Follows;
 DROP TABLE IF EXISTS Posts;
-DROP TABLE IF EXISTS Authors;
 
 CREATE TABLE Follows(
   active_author INTEGER NOT NULL,
   passive_author INTEGER NOT NULL,
   follow INTEGER NOT NULL,
-  primary key(active_author, passive_author),
+  primary key(active_author, passive_author)
 );
 
 CREATE TABLE Posts(
   post_id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   text TEXT NOT NULL,
-  time_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  time_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
