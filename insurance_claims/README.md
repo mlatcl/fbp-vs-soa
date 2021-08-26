c42847571bbb204884f057d42562eccce43bd104
Andrei Paleyes <apalees@gmail.com>
2021-08-26 11:07:32 +0100

    Renamed input data file, slightly rerouted app

--- insurance_claims/README.md
+++ insurance_claims/README.md
@@ -1,5 +1,5 @@
 A simple insurance claims processing workflow. The workflow itself is described here:
-https://www.signavio.com/post/10-workflow-examples/
+https://www.signavio.com/post/10-workflow-examples/ . We don't follow it exactly, but it is pretty close.
 
 We use Auto Insurance Claims Fraud Detection dataset (not sure where it originated from) to generate some input data:
 https://www.kaggle.com/roshansharma/insurance-claim
--- insurance_claims/main.py
+++ insurance_claims/main.py
@@ -1,6 +1,9 @@
 import sys
 import random
 import pandas as pd
+import pathlib
+
+
 
 from insurance_claims.fbp_app_min import App
 
@@ -10,12 +13,18 @@ random.seed(42)
 n_steps = 5
 n_claims_per_step = 5
 
-input_data_df = pd.read_csv("insurance_claims/input_data.csv")
+# we know csv with input data is always near this file main.py is
+# so here we retrieve main.py's directory here
+# this way resulting path is the same regardless of how and where the file is executed
+directory_path = pathlib.Path(__file__).parent.resolve()
+input_data_df = pd.read_csv(directory_path.joinpath("insurance_claims.csv"))
 input_data_df['claim_id'] = input_data_df.index
 input_data = input_data_df.to_dict(orient='records')
 
+
 app = App()
 total_amount_claimed = 0.0
+
 
 for step in range(n_steps):
     print(f"################### Iteration {step} ###################")
@@ -25,10 +34,10 @@ for step in range(n_steps):
     total_amount_claimed += sum([ir["total_claim_amount"] for ir in input_records])
     print(f"Total amount claimed: {total_amount_claimed}")
 
-    # add data and evaluate the app
+    print("--- Evaluating the app ---")
     app.add_data(input_records)
     claim_payouts = app.evaluate()
 
-    # print output
+    print("--- Processing app output ---")
     total_amount_paid = sum([cp.payout for cp in claim_payouts])
     print(f"Total amount paid: {total_amount_paid}")
