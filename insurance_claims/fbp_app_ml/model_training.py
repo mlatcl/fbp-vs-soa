50a81681c231c4c816db948be5e512c623baa01b
Andrei Paleyes <apalees@gmail.com>
2021-08-26 15:19:41 +0100

    Serialization of all model artifacts, implementation of ML app

--- insurance_claims/fbp_app_ml/model_training.py
+++ insurance_claims/fbp_app_ml/model_training.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import preprocessing
 
+
 claim_complexity_df = pd.read_csv("claim_complexity.csv")
 claim_complexity_df = claim_complexity_df.drop_duplicates(subset=["claim_id"])
 claim_complexity_df.set_index('claim_id', inplace=True)
@@ -13,6 +14,11 @@ for column_name in claim_complexity_df.columns:
     if claim_complexity_df[column_name].dtype == object:
         claim_complexity_df[column_name] = le.fit_transform(claim_complexity_df[column_name])
 
+    # we now need to save these trained encoders to be able to reuse them during prediction
+    file_name = f"training_artifacts/{column_name}_encoder.obj"
+    with open(file_name, "wb") as f:
+        pickle.dump(le, f)
+
 X = claim_complexity_df.drop("is_complex", axis=1)
 y = claim_complexity_df["is_complex"]
 
@@ -21,5 +27,5 @@ y = claim_complexity_df["is_complex"]
 model = DecisionTreeClassifier(max_depth=2).fit(X, y)
 print("Prediction score: ", model.score(X, y))
 
-with open("fbp_model.obj", "wb") as f:
+with open("training_artifacts/fbp_model.obj", "wb") as f:
     pickle.dump(model, f)
