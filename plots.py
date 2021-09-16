9e832f8365db72478fe2637ba7b536990e9ab224
cabrerac <christiancabrera89@gmail.com>
2021-09-16 17:31:31 +0100

    Adding plots for conference paper results

--- plots.py
+++ plots.py
@@ -16,33 +16,37 @@ plt.rc("ytick", labelsize=SMALL_SIZE)
 plt.rc("legend", fontsize=SMALL_SIZE)
 plt.rc("figure", titlesize=LARGE_SIZE)
 
-data = pd.read_csv("metrics.csv")
+data = pd.read_csv("results.csv")
 metrics = data.columns.tolist()
-metrics.remove("App Key")
+applications = data["App"].tolist()
+applications = list(dict.fromkeys(applications))
+metrics.remove("Key")
+metrics.remove("App")
 for metric in metrics:
-    data_fbp = data.loc[data["App Key"].isin(["fbp_app_min", "fbp_app_data", "fbp_app_ml"])]
-    data_soa = data.loc[data["App Key"].isin(["soa_app_min", "soa_app_data", "soa_app_ml"])]
-    X = ["Min", "Data", "ML"]
-    fig, ax = plt.subplots()
-    sns.lineplot(data=data_fbp, x=X, y=metric, ax=ax, label="FBP-based", marker='o', markersize="8", linestyle=':')
-    x = [0, 1, 2]
-    y = data_fbp[metric].values
-    u = np.diff(x)
-    v = np.diff(y)
-    pos_x = x[:-1] + u/2
-    pos_y = y[:-1] + v/2
-    norm = np.sqrt(u**2+v**2)
-    ax.quiver(pos_x, pos_y, u/norm, v/norm, angles="xy", zorder=5, pivot="mid")
-    sns.lineplot(data=data_soa, x=X, y=metric, ax=ax, label="SOA-based", marker='s', markersize="8", linestyle=':')
-    y = data_soa[metric].values
-    u = np.diff(x)
-    v = np.diff(y)
-    pos_x = x[:-1] + u/2
-    pos_y = y[:-1] + v/2
-    norm = np.sqrt(u**2+v**2)
-    ax.quiver(pos_x, pos_y, u/norm, v/norm, angles="xy", zorder=5, pivot="mid")
-    ax.set(xlabel="Application Stage", ylabel=metric)
-    ax.grid(linestyle="-", linewidth="0.25", color="grey")
-    ax.legend(title="Application Type", fancybox=True)
-    plt.savefig("figs/"+metric.replace(" ", "_").lower()+".pdf")
+    for application in applications:
+        data_fbp = data.loc[(data["Key"].isin(["fbp_app_min", "fbp_app_data", "fbp_app_ml"])) & (data["App"] == application)]
+        data_soa = data.loc[(data["Key"].isin(["soa_app_min", "soa_app_data", "soa_app_ml"])) & (data["App"] == application)]
+        X = ["Min", "Data", "ML"]
+        fig, ax = plt.subplots()
+        sns.lineplot(data=data_fbp, x=X, y=metric, ax=ax, label="FBP-based", marker='o', markersize="8", linestyle=':')
+        x = [0, 1, 2]
+        y = data_fbp[metric].values
+        u = np.diff(x)
+        v = np.diff(y)
+        pos_x = x[:-1] + u/2
+        pos_y = y[:-1] + v/2
+        norm = np.sqrt(u**2+v**2)
+        ax.quiver(pos_x, pos_y, u/norm, v/norm, angles="xy", zorder=5, pivot="mid")
+        sns.lineplot(data=data_soa, x=X, y=metric, ax=ax, label="SOA-based", marker='s', markersize="8", linestyle=':')
+        y = data_soa[metric].values
+        u = np.diff(x)
+        v = np.diff(y)
+        pos_x = x[:-1] + u/2
+        pos_y = y[:-1] + v/2
+        norm = np.sqrt(u**2+v**2)
+        ax.quiver(pos_x, pos_y, u/norm, v/norm, angles="xy", zorder=5, pivot="mid")
+        ax.set(xlabel="Application Stage", ylabel=metric)
+        ax.grid(linestyle="-", linewidth="0.25", color="grey")
+        ax.legend(title="Application Type", fancybox=True)
+        plt.savefig("figs-conference/"+application+"_"+metric.replace(" ", "_").lower()+".png")
 
